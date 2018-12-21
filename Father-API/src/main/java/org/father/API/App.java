6e45cb257fb41c3beea693d54cfbe2b4e23cdbed
haitao.zhang <18911626826@163.com>
2018-12-21 15:58:23 +0800

    提交设置Banner方法

--- Father-API/src/main/java/org/father/API/App.java
+++ Father-API/src/main/java/org/father/API/App.java
@@ -1,19 +1,26 @@
 package org.father.API;
 
+import java.io.PrintStream;
+
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * Hello world!
  *
  */
 //@EnableJpaRepositories
-@SpringBootApplication
+@SpringBootApplication()
 public class App
 {
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
-    	SpringApplication.run(App.class, args);
+    	
+    	//SpringApplication.run(App.class, args);方法一：最普通的方法，用来启动springboot应用
+    	SpringApplication sal=new SpringApplication(App.class);//方法二：设置Banner的方法
+    	sal.setBannerMode(Banner.Mode.CONSOLE);
+    	sal.run(args);
     }
 }
